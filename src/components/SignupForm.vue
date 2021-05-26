21cf913fcfe8db863ac5c5fbfd2b304aa8b5c475
Han Kuo <han.kuo@ses-imagotag.com>
2021-05-26 13:06:42 +0800

    feat: example of checkbox

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -11,10 +11,31 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Accept terms and conditions</label>
+    </div>
+
+    <div>
+      <input type="checkbox" value="Han" v-model="names">
+      <label>Han</label>
+    </div>
+    <div>
+      <input type="checkbox" value="So" v-model="names">
+      <label>SO</label>
+    </div>
+    <div>
+      <input type="checkbox" value="Lo" v-model="names">
+      <label>LO</label>
+    </div>
   </form>
+
   <p>email: {{ email }}</p>
   <p>password: {{ password }}</p>
   <p>role: {{ role }}</p>
+  <p>terms: {{ terms }}</p>
+  <p>names: {{ names }}</p>
 </template>
 
 <script>
@@ -23,7 +44,9 @@ export default {
     return {
       email: '',
       password: '',
-      role: ''
+      role: '',
+      terms: false,
+      names: []
     }
   }
 }
@@ -55,5 +78,12 @@ export default {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
   }
 </style>
