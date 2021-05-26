7b255005ac50dfc639f5bc4d089db8c4e8ecd4bf
Han Kuo <han.kuo@ses-imagotag.com>
2021-05-26 12:53:18 +0800

    feat: example of select option

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,9 +5,16 @@
 
     <label>Password:</label>
     <input type="password" required v-model="password">
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
   </form>
   <p>email: {{ email }}</p>
   <p>password: {{ password }}</p>
+  <p>role: {{ role }}</p>
 </template>
 
 <script>
@@ -15,7 +22,8 @@ export default {
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      role: ''
     }
   }
 }
@@ -39,7 +47,7 @@ export default {
     letter-spacing: 1px;
     font-weight: bold;
   }
-  input {
+  input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;
